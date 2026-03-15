247db1c248f650d1af808c7d1aa84769cd368db6
Adarsh <2020cs_adarshkpoojary_a@nie.ac.in>
2026-03-15 22:39:22 +0530

    Marquee1 "key" fix

--- src/app/components/Marquee1.jsx
+++ src/app/components/Marquee1.jsx
@@ -6,12 +6,12 @@ export default function Marquee1() {
 
       <div className="marquee flex gap-6 text-xl font-extralight italic text-gray-300">
 
-        {Array(10).fill(
-          <span className="flex items-center gap-3">
-            <div className="h-2 w-2 bg-emerald-300 rounded-2xl animate-pulse"></div>
-           FULL STACK  DEVELOPER  
-          </span>
-        )}
+            {[...Array(10)].map((_, i) => (
+        <span key={i} className="flex items-center gap-3">
+          <div className="h-2 w-2 bg-emerald-300 rounded-2xl animate-pulse"></div>
+          FULL STACK DEVELOPER
+        </span>
+      ))}
 
       </div>
 
