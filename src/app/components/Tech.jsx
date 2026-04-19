8873e8abbf3080a61ababc16386237d41e93acd3
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2026-04-19 19:49:25 +0530

    Update Tech.jsx

--- src/app/components/Tech.jsx
+++ src/app/components/Tech.jsx
@@ -20,6 +20,7 @@ const item = {
   }
 };
   return (
+    
     <motion.div variants={item} whileHover={{ scale: 1.05 }} className="flex items-center gap-3 text-lg text-gray-100 opacity-80 hover:opacity-100 transition font-extralight italic will-change-transform hover:drop-shadow-[0_0_10px_rgba(255,255,255,0.6)] transition duration-300 hover:scale-120">
       <img src={icon} className="w-10 h-10" />
       <span className="font-(--font-inter) text-2xl">{name}</span>
