befee44614031affe8e1a15c48d219753a719263
adarsh_k_poojary <108194966+adarshk0511@users.noreply.github.com>
2026-05-15 20:03:47 +0530

    Remove empty line in SectionLabel component

--- src/app/components/SectionLabel.jsx
+++ src/app/components/SectionLabel.jsx
@@ -4,7 +4,6 @@ import { motion } from "framer-motion";
 
 export default function SectionLabel({ text }) {
   return (
-    
     <div className="flex items-center gap-3 text-gray-300 text-2xl ml-[20%] mb-[2rem]">
 
       <motion.span
